185087867af4cc90d1400b76960ebba144a867ca
alan <alan.wei@live.com>
2021-03-01 15:11:21 +0800

    type: 修改定义文件

--- index.d.ts
+++ index.d.ts
@@ -4,27 +4,28 @@ export interface WhistleOptions extends PlainObject {
     /**
      * 调试模式
      */
-    debugMode: boolean
+    debugMode?: boolean
     /**
      * 代理监听端口号
      */
-    port: number
+    port?: number
     /**
      * 数据存储目录
      */
-    baseDir: string
-    copy: string
+    baseDir?: string
+    copy?: string
     /**
      * 证书目录
      */
-    certDir: string
+    certDir?: string
     /**
      * 数据订阅JS模块地址
      */
-    subscriber: string
-    mode: "capture" | string
+    subscriber?: string
+    mode?: "capture" | string
 }
 
-
-const whistle: (options: WhistleOptions, callback: () => void) => void
-export default whistle;
+declare module "whistle-extend" {
+    const whistle: (options: WhistleOptions, callback: () => void) => void
+    export default whistle;
+}
